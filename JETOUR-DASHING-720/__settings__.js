a04a3f0f57f4dee8a4e10f51e0700572729c6738
amam4web-afk <amam4web@gmail.com>
2025-10-07 21:44:37 +0300

    Add files via upload

---
+++ JETOUR-DASHING-720/__settings__.js
@@ -0,0 +1,21 @@
+window.ASSET_PREFIX = "";
+window.SCRIPT_PREFIX = "";
+window.SCENE_PATH = "1562743.json";
+window.CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': true,
+    'preserveDrawingBuffer': false,
+    'preferWebGl2': true,
+    'powerPreference': "default"
+};
+window.SCRIPTS = [ 106371787, 106371869, 106372026, 106371950, 106371953, 106371948, 106371954, 106371807, 106372004, 106371809, 106371936, 106371873, 106876257, 108152854, 108525865, 108895141, 109019423, 109189437, 109203563, 109203566, 109203652, 109203660, 109208759, 109208804, 109465997, 109604555, 109607748, 109607749, 109638837, 109672900, 109692011, 109933297, 109936073, 110850508 ];
+window.CONFIG_FILENAME = "config.json";
+window.INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: true,
+    useTouch: true
+};
+pc.script.legacy = false;
+window.PRELOAD_MODULES = [
+];
